2e139c7f5ebe11f80508bb39b2459d4ec4833db1
Gabriel432135 <gabriel432135@hotmail.com>
2025-11-22 18:56:42 -0300

    UI

--- app/src/main/java/com/example/heroes/MainActivity.kt
+++ app/src/main/java/com/example/heroes/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.heroes
 
-import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,12 +16,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -40,20 +39,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HeroesTheme {
-
+                HeroApp()
             }
         }
     }
 }
 
 @Composable
-fun HeroList(modifier: Modifier = Modifier) {
-    LazyColumn( modifier = modifier
-        .padding(16.dp)) {
-        items(heroes){
-            HeroItem(hero = it)
+fun HeroApp(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar()
+        }
+    ) {innerPadding ->
+        LazyColumn( contentPadding = innerPadding, modifier = modifier
+            .padding(16.dp)) {
+            items(heroes){
+                HeroItem(hero = it)
+                Spacer(modifier = Modifier.height(8.dp))
+            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier){
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = {
+            Text(stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge)
+        }
+    )
 }
 
 @Composable
@@ -61,7 +80,7 @@ fun HeroItem(modifier: Modifier = Modifier, hero: Hero){
 
 
     Card(modifier = modifier
-        .height(88.dp)
+        .height(104.dp)
         .fillMaxWidth()
         .clip(MaterialTheme.shapes.small),
         elevation = CardDefaults.cardElevation(
@@ -70,21 +89,21 @@ fun HeroItem(modifier: Modifier = Modifier, hero: Hero){
 
     {
         Row(modifier = Modifier.padding(16.dp)){
-            Column {
+            Column(modifier = Modifier.weight(1f)){
                 Text(text = stringResource(hero.nameRes),
                     style = MaterialTheme.typography.displaySmall)
                 Text(text = stringResource(hero.descriptionRes),
                     style = MaterialTheme.typography.bodyLarge)
             }
-            Spacer(Modifier.weight(2f).width(16.dp))
-
+            Spacer(Modifier.width(16.dp))
             Image(
                 modifier =
                     Modifier
                         .clip(MaterialTheme.shapes.small)
-                        .height(72.dp),
-                //painter = rememberAsyncImagePainter(painterResource(hero.imageRes)),
-                painter = painterResource(hero.imageRes),
+                        .height(72.dp)
+                        .width(72.dp),
+                painter = rememberAsyncImagePainter(model = hero.imageRes),
+                //painter = painterResource(hero.imageRes),
                 contentDescription = null)
 
         }
@@ -95,6 +114,6 @@ fun HeroItem(modifier: Modifier = Modifier, hero: Hero){
 @Composable
 fun HeroPreview() {
     HeroesTheme {
-        HeroList()
+        HeroApp()
     }
 }
